7f1ce15e73452e15366d9f4794da734ad0b7131c
Gabriel <gabriel.ne@hotmail.com>
2025-04-26 20:53:50 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,10 +3,20 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 
+
+@Entity
+@Table (name = "tb_user")
+public class User implements Serializable {
 		private static final long serialVersionUID = 1L;
 		
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long Id = null;
 		private String Name = null;
 		private String Email = null;
