9f7e643860dc465dc70f6198c0681f1719449376
Gabriel <gabriel.ne@hotmail.com>
2025-04-30 17:00:56 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -9,29 +9,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
-
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable {	
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	
-	private Set<Category> categories = new HashSet<>();
-	
-	public Product() {
-	}
 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+	}	
+
+	
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
@@ -40,6 +44,9 @@ public class Product implements Serializable {
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
+
+
+
 
 	public long getId() {
 		return id;
@@ -101,7 +108,5 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return id == other.id;
 	}
-	
-	
 
 }
