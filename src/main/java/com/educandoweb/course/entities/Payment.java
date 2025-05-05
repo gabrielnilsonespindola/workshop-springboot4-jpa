e77a9907fc1306fb736491d501fa339288e3c15f
Gabriel <gabriel.ne@hotmail.com>
2025-05-05 15:40:08 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -2,7 +2,8 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
-import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,22 +13,21 @@ import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
-@Table (name = "tb_payment")
-public class Payment implements Serializable {	
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	
-	
+
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
-	
+
 	public Payment() {
 	}
 
@@ -64,7 +64,10 @@ public class Payment implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -76,10 +79,11 @@ public class Payment implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Payment other = (Payment) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
-
 }
